3d3fcba5b8c37fc14ddac645b44e944125f01fc8
amico <amico8amico@gmail.com>
2016-02-01 17:51:11 +0800

    パスワードの桁数エラー表示

--- join/index.php
+++ join/index.php
@@ -119,6 +119,9 @@ if (isset($_POST) && !empty($_POST)) {
               <?php if(isset($error['password']) && $error['password'] == 'blank'): ?>
               <p class="error">* パスワードを入力してください。</p>
               <?php endif; ?>
+              <?php if(isset($error['password']) && $error['password'] == 'length'): ?>
+              <p class="error">* パスワードは4文字以上で入力してください。</p>
+              <?php endif; ?>
             </div>
           </div>
           <!-- プロフィール写真 -->
